75781d2a4f09f9f938462da939cf27313a05d31e
nicolanzi1 <nicolanzi@gmail.com>
2020-10-21 19:38:53 +0100

    Wrote errorMiddleware to handle product errors instead of doing it in server.js, which also allows me to throw a new error message. Also added a store and set up redux devtools to the frontend and passed it inside index through Provider with react-redux

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -47,7 +47,7 @@ const ProductScreen = ({ match }) => {
                 <Row>
                   <Col>Price:</Col>
                   <Col>
-                    <strong>${product.price}</strong>
+                    <strong>£{product.price}</strong>
                   </Col>
                 </Row>
               </ListGroup.Item>
