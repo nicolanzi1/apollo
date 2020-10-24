59a56339f4c2a835e95e78d46b76998055869078
nicolanzi1 <nicolanzi@gmail.com>
2020-10-24 23:03:30 +0100

    Wrote userUpdate constants, actions and reducer, passed it into the store and added it to the UserEditScreen so that Admin Users are able to check, update, delete and grant admin capabilities to other users from the Admin Dropdown

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -149,9 +149,7 @@ const updateUser = asyncHandler(async (req, res) => {
   if (user) {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
-    if (user && user.isAdmin) {
-      user.isAdmin = req.body.isAdmin;
-    }
+    user.isAdmin = req.body.isAdmin;
 
     const updatedUser = await user.save();
 
